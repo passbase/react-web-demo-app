cc367c1cf31b1c84c42fb56c2b5bf514c22f2365
Mathias Klenk <mathias@passbase.com>
2019-12-15 22:17:00 -0500

    Finished the React Demo App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import Passbase from "@passbase/button";
+import VerifyButton from "@passbase/button/react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-        
+      <VerifyButton
+        apiKey={"YOUR_PUBLISHABLE_API_KEY"}
+        onFinished={(error, authKey, additionalAttributes) => {}}
+        additionalAttributes={{ }}
+        prefillAttributes={{ }}
+        theme={{ }}
+      />;
         
       </header>
     </div>
