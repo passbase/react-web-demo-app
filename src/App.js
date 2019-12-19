a2d02631f9bc3e408223fd49f7852d46c34489c3
Mathias Klenk <mathias@passbase.com>
2019-12-19 11:41:50 -0500

    Latest Updated with customizations and without

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
+      {/* Without customizations */}       
       <VerifyButton
         apiKey={"YOUR_PUBLISHABLE_API_KEY"}
         onFinished={(error, authKey, additionalAttributes) => {}}
@@ -15,7 +16,28 @@ function App() {
         prefillAttributes={{ }}
         theme={{ }}
       />;
-        
+       
+      {/* With customizations */}       
+      {/* 
+      <VerifyButton
+        apiKey={"YOUR_PUBLISHABLE_API_KEY"}
+        onFinished={(error, authKey, additionalAttributes) => {}}
+        additionalAttributes={{
+          customer_user_id: "a2ab4-f2jc4-k2dd4-1fa3x",
+          another_attribute: ""
+        }}
+        prefillAttributes={{
+          email: "user@test.com",
+          country: "de"
+        }}
+        theme={{
+          accentColor: "#ff0000",
+          font: "Exo", 
+          darkMode: true
+        }}
+      />;
+      */}
+
       </header>
     </div>
   );
